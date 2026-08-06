b9cd8429510a3a895ffcd382d64b01298e37bb2c
chtoetolol4 <from1x228a409@gmail.com>
2026-08-06 14:02:33 +0500

    06.08.28

--- Assets/EndlessController.cs
+++ Assets/EndlessController.cs
@@ -2,21 +2,28 @@ using UnityEngine;
 
 public class EndlessController : MonoBehaviour
 {
-    private float []IslandsPositionZ;
+    private float[] IslandsPositionZ = new float[5];
     public GameObject Island;
-    private Vector3 IslandRotation;
+    private float Multiplier = 1;
     void Start()
     {
-        // IslandRotation = Island.transform.rotation;
         for (int i = 0; i < 5; i++)
         {
             Debug.Log("Число: " + i);
-            IslandsPositionZ[i] = Random.Range(0, 5);
-            Instantiate(Island, new Vector3(0,0,i), Quaternion.identity);
+            IslandsPositionZ[i] = Random.Range(0, 5f);
+            if (IslandsPositionZ[i] >= 2f)
+            {
+                Multiplier = -1;
+            }
+            else
+            {
+                Multiplier = 1;
+            }
+            Instantiate(Island, new Vector3(i + IslandsPositionZ[i] * Multiplier,0,i + IslandsPositionZ[i]), Quaternion.identity);
         } 
     }
 
-    // Update is called once per frame
+    
     void Update()
     {
 

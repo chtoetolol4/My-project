649de7ae484e026aa8957d956b5afffa707d303e
chtoetolol4 <from1x228a409@gmail.com>
2026-08-11 19:35:00 +0500

    страдаем с математикой

--- Assets/EndlessController.cs
+++ Assets/EndlessController.cs
@@ -5,6 +5,8 @@ public class EndlessController : MonoBehaviour
     private float[] IslandsPositionZ = new float[5];
     public GameObject Island;
     private float Multiplier = 1;
+    private float DeltaY;
+    private float MultiplierY = 1;
     void Start()
     {
         for (int i = 0; i < 5; i++)
@@ -19,7 +21,15 @@ public class EndlessController : MonoBehaviour
             {
                 Multiplier = 1;
             }
-            Instantiate(Island, new Vector3(i + IslandsPositionZ[i] * Multiplier,0,i + IslandsPositionZ[i]), Quaternion.identity);
+            if (i > 3)
+            {
+                MultiplierY = -1f;
+            }
+            else
+            {
+                MultiplierY = 1;
+            }
+            Instantiate(Island, new Vector3(i + IslandsPositionZ[i] * Multiplier,MultiplierY * DeltaY,i + IslandsPositionZ[i]), Quaternion.identity);
         } 
     }
 

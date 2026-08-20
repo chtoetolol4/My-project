c5713756d43ba007cb51b3cb68b00b4ab2f1a90a
chtoetolol4 <from1x228a409@gmail.com>
2026-08-20 14:01:59 +0500

    20.08.26 v2

---
+++ Assets/RandomNubers.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class RandomNubers : MonoBehaviour
+{
+    float Number = 2;
+    float M = 1;
+    int Count = 1;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

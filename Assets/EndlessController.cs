b86d2110db72a9975d739be5e8a7a3384f13bce7
chtoetolol4 <from1x228a409@gmail.com>
2026-08-20 13:50:33 +0500

    20.08.26

--- Assets/EndlessController.cs
+++ Assets/EndlessController.cs
@@ -22,11 +22,16 @@ public class EndlessController : MonoBehaviour
                 Multiplier = 1;
             }
             DeltaY = Random.Range(0.5f, 2f);
-            if (i > 4)
-            {
-                Island.GetComponent<MeshRenderer>().material = VirusMaterial;
-            }
             Instantiate(Island, new Vector3(i * 10 + IslandsPositionZ[i] * Multiplier,(i + IslandsPositionZ[i] * Multiplier) * 0.15f, i * 10 + IslandsPositionZ[i]), Quaternion.identity);
+            if (i == 4)
+            {
+                Transform childTransform = Island.transform.Find("floe.011");
+                    if (childTransform != null)
+                    {
+                        GameObject childObject = childTransform.gameObject;
+                        childObject.GetComponent<MeshRenderer>().material = VirusMaterial;
+                    }
+            }
         } 
     }
 

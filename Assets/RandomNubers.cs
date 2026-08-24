c623bda7c357f0753b1ba275805d0012a5bfdfc9
chtoetolol4 <from1x228a409@gmail.com>
2026-08-24 22:59:19 +0500

    почемутоНеРаботает

--- Assets/RandomNubers.cs
+++ Assets/RandomNubers.cs
@@ -5,9 +5,26 @@ public class RandomNubers : MonoBehaviour
     float Number = 2;
     float M = 1;
     int Count = 1;
+    int DeciderRandom = 1;
     void Start()
     {
-        
+        for (Count = 0; Count < 6; Count++)
+        {
+            M = Random.Range(0.3f, 3f);
+            DeciderRandom = Random.Range(1, 2);
+            if (DeciderRandom == 1)
+            {
+                Debug.Log(Number / M);
+            }
+            else
+            {
+                Debug.Log(Number * M);
+            }
+            if (Count > 3)
+            {
+                Number = 1;
+            }
+        }
     }
 
     // Update is called once per frame

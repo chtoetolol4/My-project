61752e497d8cb8664e0f27d3d692e86eace950e6
chtoetolol4 <from1x228a409@gmail.com>
2026-08-25 19:12:40 +0500

    умный друг нам помог

--- Assets/EndlessController.cs
+++ Assets/EndlessController.cs
@@ -8,6 +8,7 @@ public class EndlessController : MonoBehaviour
     private float DeltaY;
     public Material VirusMaterial;
     public Material HealthyMaterial;
+    
     void Start()
     {
         for (int i = 0; i < 5; i++)
@@ -22,19 +23,31 @@ public class EndlessController : MonoBehaviour
                 Multiplier = 1;
             }
             DeltaY = Random.Range(0.5f, 2f);
-            Instantiate(Island, new Vector3(i * 10 + IslandsPositionZ[i] * Multiplier,(i + IslandsPositionZ[i] * Multiplier) * 0.15f, i * 10 + IslandsPositionZ[i]), Quaternion.identity);
-            if (i == 4)
+            
+            // Создаем экземпляр острова
+            GameObject newIsland = Instantiate(Island, new Vector3(i * 10 + IslandsPositionZ[i] * Multiplier, (i + IslandsPositionZ[i] * Multiplier) * 0.15f, i * 10 + IslandsPositionZ[i]), Quaternion.identity);
+            
+            // Ищем дочерний объект в СОЗДАННОМ экземпляре, а не в префабе
+            Transform childTransform = newIsland.transform.Find("floe.011");
+            if (childTransform != null)
             {
-                Transform childTransform = Island.transform.Find("floe.011");
-                    if (childTransform != null)
-                    {
-                        GameObject childObject = childTransform.gameObject;
-                        childObject.GetComponent<MeshRenderer>().material = VirusMaterial;
-                    }
+                GameObject childObject = childTransform.gameObject;
+                MeshRenderer renderer = childObject.GetComponent<MeshRenderer>();
+                
+                // Проверяем каждый пятый остров (индексы: 4, 9, 14, 19...)
+                if (i % 5 == 4)
+                {
+                    // ВАЖНО: создаем новый экземпляр материала, чтобы не менять оригинальный префаб
+                    renderer.material = new Material(VirusMaterial);
+                }
+                else
+                {
+                    // Для остальных тоже создаем новый экземпляр HealthyMaterial
+                    renderer.material = new Material(HealthyMaterial);
+                }
             }
         } 
     }
-
     
     void Update()
     {
